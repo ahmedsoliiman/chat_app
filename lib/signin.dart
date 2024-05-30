ebdc69541a91f0757b69b79333d6b285288afd2c
Ahmed Soliman <66446508+ahmedsoliiman@users.noreply.github.com>
2024-05-30 20:20:49 +0300

    final project

---
+++ lib/signin.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:food_app/signup.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      resizeToAvoidBottomInset: false,
+      body:  Container(
+        child: Stack(
+          children: [
+             Container(
+              height: MediaQuery.of(context).size.height / 4.0,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Color.fromARGB(255, 38, 1, 97),
+                    Color.fromARGB(255, 12, 29, 109),
+                  ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+                  borderRadius: BorderRadius.vertical(
+                      bottom: Radius.elliptical(
+                          MediaQuery.of(context).size.width, 106.0))),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 70.0),
+              child: Column(
+                children: [
+                  Center(
+                      child: Text(
+                    "Sign In",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold),
+                  )),
+                  Center(
+                      child: Text(
+                    "Login to your account",
+                    style: TextStyle(
+                        color: Color(0xFFbbb0ff),
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500),
+                  )),
+                  SizedBox(height: 20.0),
+                  Container(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+                    child: Material(
+                      elevation: 5.0,
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 30.0, horizontal: 20.0),
+                        height: MediaQuery.of(context).size.height / 2,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Email",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w500)),
+                            SizedBox(height: 10.0),
+                            Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 1.0, color: Colors.black38)),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    prefixIcon: Icon(
+                                      Icons.mail_outline,
+                                      color: Color(0xFF7f30fe),
+                                    )),
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            Text("Password",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w500)),
+                            SizedBox(height: 10.0),
+                            Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 1.0, color: Colors.black38)),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    prefixIcon: Icon(
+                                      Icons.remove_red_eye_outlined,
+                                      color: Color(0xFF7f30fe),
+                                    )),
+                                obscureText: true,
+                              ),
+                            ),
+                            SizedBox(height: 10.0),
+                            Container(
+                              alignment: Alignment.bottomRight,
+                              child: Text("Forget Password?",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w500)),
+                            ),
+                            SizedBox(height: 50.0),
+                            Container(
+                              child: Center(
+                                child: Container(
+                                  width: 130,
+                                  child: Material(
+                                    elevation: 5.0,
+                                    child: Container(
+                                      padding: EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                          color:
+                                              Color.fromARGB(255, 11, 32, 123),
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: Center(
+                                          child: Text(
+                                        "Sign In",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.bold),
+                                      )),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      (Text("Dont't have an account?")),
+                      InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SignUp()),
+                            );
+                          },
+                          child: (Text("Sign Up Now!"))),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
